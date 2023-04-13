48da8545785a983ce8b1f6e51c0c73dead078cbf
Bob Oyier <boboyier92@gmail.com>
2023-04-13 21:45:27 +0300

    13th April, 2023

---
+++ 13-04-2023/remove_anchor_from_url.rb
@@ -0,0 +1,13 @@
+=begin
+
+ Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+ "www.codewars.com#about" --> "www.codewars.com"
+ "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+=end
+
+def remove_url_anchor(url)
+  url.split('#')[0]
+end
