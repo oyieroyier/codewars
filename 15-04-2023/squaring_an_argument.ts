546ffbeca68ac67f5a01674335613df0985146b1
Bob Oyier <boboyier92@gmail.com>
2023-04-16 15:32:16 +0300

    15th April, 2023

---
+++ 15-04-2023/squaring_an_argument.ts
@@ -0,0 +1,9 @@
+/* 
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+export function square(a: number): number {
+	return a * a;
+}
---
+++ 15-04-2023/you_cant_code_under_pressure.rb
@@ -0,0 +1,9 @@
+=begin
+
+ Code as fast as you can! You need to double the integer and return it.
+
+=end
+
+def double_integer(i)
+  i * 2
+end
