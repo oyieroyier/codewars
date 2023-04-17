3416c6b5eba1a5159b77626edda357e2ff77c27d
Bob Oyier <boboyier92@gmail.com>
2023-04-17 21:39:57 +0300

    17th April, 2023

---
+++ 17-04-2023/between_extremes.ts
@@ -0,0 +1,24 @@
+/*
+
+ Given an array of numbers, return the difference between the largest and smallest values.
+
+For example:
+
+ [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+ [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+ The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+*/
+
+// Submitted solution
+function betweenExtremes(numbers: number[]): number {
+	const sorted = numbers.sort((a, b) => a - b);
+	return sorted[sorted.length - 1] - sorted[0];
+}
+
+// Other implementation
+function betweenExtreme(numbers) {
+	return Math.max(...numbers) - Math.min(...numbers);
+}
---
+++ 17-04-2023/sum_even_numbers.ts
@@ -0,0 +1,35 @@
+/*
+
+ Complete the function that takes a sequence of numbers as single parameter.
+ Your function must return the sum of the even values of this sequence.
+
+ Only numbers without decimals like 4 or 4.0 can be even.
+
+ The input is a sequence of numbers: integers and/or floats.
+
+Examples
+ [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+ []    
+
+*/
+// Initial submitted solution.
+function sumEvenNumbers(input: number[]): number {
+	return input.length
+		? input
+				.filter((number) => number % 2 === 0)
+				.reduce((sum, number) => sum + number)
+		: 0;
+}
+
+// Refactored to this:
+// Remember to set the initial value of the reduce method to 0.
+function sumEvenNumber(input: number[]): number {
+	return input
+		.filter((number) => number % 2 === 0)
+		.reduce((sum, number) => sum + number, 0);
+}
+
+// Better implementations:
+// Run the filter inside the reduce method.
+const sumEvenNumb = (a: number[]): number =>
+	a.reduce((r, e) => r + (e % 2 ? 0 : e), 0);
