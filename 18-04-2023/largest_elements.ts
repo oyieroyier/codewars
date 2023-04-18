6d8253934d32116ad4d346ba51e9e038ebf9b128
Bob Oyier <boboyier92@gmail.com>
2023-04-18 13:43:04 +0300

    18th April, 2023

---
+++ 18-04-2023/largest_elements.ts
@@ -0,0 +1,11 @@
+/*
+ Write a program that outputs the top n elements from a list.
+
+ Example:
+
+ largest(2, [7,6,5,4,3,2,1]) // => [6,7]
+*/
+
+function largest(n: number, array: number[]): number[] {
+	return array.sort((a, b) => a - b).splice(array.length - n);
+}
