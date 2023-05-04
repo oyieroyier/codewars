98b2a472be4c84aa63eb9c46991face92836085f
Bob Oyier <boboyier92@gmail.com>
2023-05-05 02:06:03 +0300

    5th May, 2023

---
+++ 05-05-2023/fire_on_the_boat.ts
@@ -0,0 +1,16 @@
+/*
+
+ Enjoying your holiday, you head out on a scuba diving trip!
+
+ Disaster!! The boat has caught fire!!
+
+ You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+ Go to work!
+
+*/
+
+export const removeFire = (str: string): string => str.replace(/Fire/gi, '~~');
+
+// Alternative solution:
+export const removeFir = (str: string): string => str.replaceAll('Fire', '~~');
